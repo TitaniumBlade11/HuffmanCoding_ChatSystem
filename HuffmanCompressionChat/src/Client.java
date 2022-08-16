82844a5e0935d2be2a013d4f28d5ef04055466b1
siddharth chakravorty <siddharth.chakravorty025@gmail.com>
2022-08-16 17:53:12 -0400

    Documented code for Client and Server.

--- HuffmanCompressionChat/src/Client.java
+++ HuffmanCompressionChat/src/Client.java
@@ -1,50 +1,80 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
+// In our implementation, client always starts the conversation when it connects to a server, the
+// communication is a 2 way chat system where the chance for sending chat is done in a sea-saw
+// fashion i.e client sends msg then server then client this is to demonstrate the functionality of
+// Huffman compression more accurately for 1 block of text message(block is variable length that
+// ends with a new line character) at a time as the aim of this project is to create a POC for a
+// duplex chat system using Huffman compression-decompression algorithm for space efficiency.
+
+/**
+ * This class represents the set of functionalities to establish communication with the chat server.
+ */
 public class Client {
-  public static void main(String[] args) throws Exception {
-    Socket localClient = new Socket("localhost", 10000);
-    System.out.println("Connecting to server....");
+  public static void main(String[] args) {
+    try {
+      // Bind the socket to a local port.
+      Socket localClient = new Socket("localhost", 10000);
+      System.out.println("Connecting to server....");
 
-    OutputStream outputStream = localClient.getOutputStream();
-    InputStream inputStream = localClient.getInputStream();
+      // Get the I/O streams connected to the local socket.
+      OutputStream outputStream = localClient.getOutputStream();
+      InputStream inputStream = localClient.getInputStream();
 
-    BufferedReader bufferedInput = new BufferedReader(new InputStreamReader(inputStream));
-    Scanner consoleInput = new Scanner(System.in);
+      // Input pipe from console.
+      Scanner consoleInput = new Scanner(System.in);
 
-    DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+      String textTyped = null;
+      String textFromOtherSide = null;
 
-    String textTyped = null;
-    String textFromOtherSide = null;
-    HuffmanCoding huffmanCoding = new HuffmanCoding();
-    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+      // Creating an object of HuffmanCoding to leverage its utilities to encode and decode the
+      // strings.
+      HuffmanCoding huffmanCoding = new HuffmanCoding();
 
-    while (true) {
-      textTyped = consoleInput.nextLine();
-      CompressedPackage compressedPackage = huffmanCoding.encode(textTyped);
-      objectOutputStream.writeObject(compressedPackage);
+      // To push the serialized object in the output stream pipe of the local socket to remote end.
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 
-      if ("BYE".equals(textTyped))
-        break;
+      // To receive serialized object from the other end using input stream of the local socket.
+      ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-      CompressedPackage incomingCompressedPackage = (CompressedPackage) objectInputStream.readObject();
-      textFromOtherSide = huffmanCoding.decode(incomingCompressedPackage.decodeTreeNodeRoot,
-              incomingCompressedPackage.compressedString);
+      while (true) {
+        // Input from local user.
+        textTyped = consoleInput.nextLine();
+        // Pack the encoded string and its tree in a CompressedPackage object.
+        CompressedPackage compressedPackage = huffmanCoding.encode(textTyped);
 
-      System.out.println("Text from other side: " + textFromOtherSide);
+        // Push the compressed object through the object output stream pipeline to remote server.
+        objectOutputStream.writeObject(compressedPackage);
 
-      if (textFromOtherSide.contains("BYE"))
-        break;
-    }
+        // If the local client types "BYE" in console, break out of the loop.
+        if ("BYE".equals(textTyped))
+          break;
 
+        // Read and deserialize the incoming CompressedPackage object from the remote end.
+        CompressedPackage incomingCompressedPackage = (CompressedPackage) objectInputStream
+                .readObject();
+
+        // Get the incoming text after decoding the compressed encoded string.
+        textFromOtherSide = huffmanCoding.decode(incomingCompressedPackage.decodeTreeNodeRoot,
+                incomingCompressedPackage.compressedString);
+
+        System.out.println("Text from other side: " + textFromOtherSide);
+
+        // If remote party send "BYE" message, break out of the loop.
+        if (textFromOtherSide.contains("BYE"))
+          break;
+      }
+
+      // Safely release the resources occupied by the local client socket.
       localClient.close();
+    } catch (Exception e) {
+      System.out.println("Could not establish successful chat communication with the server.");
+    }
   }
 }
--- HuffmanCompressionChat/src/Server.java
+++ HuffmanCompressionChat/src/Server.java
@@ -2,41 +2,80 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
+// In our implementation, client always starts the conversation when it connects to a server, the
+// communication is a 2 way chat system where the chance for sending chat is done in a sea-saw
+// fashion i.e client sends msg then server then client this is to demonstrate the functionality of
+// Huffman compression more accurately for 1 block of text message(block is variable length that
+// ends with a new line character) at a time as the aim of this project is to create a POC for a
+// duplex chat system using Huffman compression-decompression algorithm for space efficiency.
+
+/**
+ * This class represents the set of functionalities for a chat server to establish connection and
+ * further communicate with the clients.
+ */
 public class Server {
-  public static void main(String[] args) throws Exception {
-    ServerSocket socket = new ServerSocket(10000);
-    Socket localClient = socket.accept();
-    System.out.println("Got connected to the other side.");
-
-    OutputStream outputStream = localClient.getOutputStream();
-    InputStream inputStream = localClient.getInputStream();
-
-    Scanner consoleInput = new Scanner(System.in);
-    DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-
-    String textIncoming = null;
-    String ourText = null;
-    HuffmanCoding huffmanCoding = new HuffmanCoding();
-    ObjectInputStream objectInputStream = new ObjectInputStream(localClient.getInputStream());
-    ObjectOutputStream objectOutputStream = new ObjectOutputStream(localClient.getOutputStream());
-
+  public static void main(String[] args) {
+    ServerSocket socket = null;
+    Socket localClient = null;
     try {
+      // Create a server socket that listens to connection requests from remote client.
+      socket = new ServerSocket(10000);
+
+      // Create a local client socket for establishing communication with remote client to enable 2
+      // way chat.
+      localClient = socket.accept();
+      System.out.println("Got connected to the other side.");
+
+      // Get the I/O streams connected to the local socket.
+      OutputStream outputStream = localClient.getOutputStream();
+      InputStream inputStream = localClient.getInputStream();
+
+      // Input pipe from console.
+      Scanner consoleInput = new Scanner(System.in);
+      DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+      String textIncoming = null;
+      String ourText = null;
+
+      // Creating an object of HuffmanCoding to leverage its utilities to encode and decode the
+      // strings.
+      HuffmanCoding huffmanCoding = new HuffmanCoding();
+
+      // To push the serialized object in the output stream pipe of the local socket to remote end.
+      ObjectInputStream objectInputStream = new ObjectInputStream(localClient.getInputStream());
+
+      // To receive serialized object from the other end using input stream of the local socket.
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(localClient.getOutputStream());
+
       while (true) {
+        // Read and deserialize the incoming CompressedPackage object from the remote end.
         CompressedPackage compressedPackage = (CompressedPackage) objectInputStream.readObject();
-        textIncoming = huffmanCoding.decode(compressedPackage.decodeTreeNodeRoot, compressedPackage.compressedString);
+
+        // Get the incoming text after decoding the compressed encoded string.
+        textIncoming = huffmanCoding.decode(compressedPackage.decodeTreeNodeRoot, compressedPackage
+                .compressedString);
 
         System.out.println(textIncoming);
 
+        // Input from local user.
         ourText = consoleInput.nextLine();
+
+        // Pack the encoded string and its tree in a CompressedPackage object.
         CompressedPackage sendCompressedPackage = huffmanCoding.encode(ourText);
+
+        // Push the compressed object through the object output stream pipeline to remote client.
         objectOutputStream.writeObject(sendCompressedPackage);
       }
+    } catch (IOException ioe) {
+      System.out.println("Could not establish successful connection pipeline to remote client.");
     } catch (Exception e) {
       System.out.println("All clients disconnected, stopping server.");
-      localClient.close();
-      socket.close();
+      try {
+        localClient.close();
+        socket.close();
+      } catch(IOException ioe) {
+        System.out.println("Could not close the socket.");
+      }
     }
-
   }
-
 }
