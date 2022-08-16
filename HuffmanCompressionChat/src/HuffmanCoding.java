b8a374cdf6ecfe8e833050748ba5083295d01325
siddharth chakravorty <siddharth.chakravorty025@gmail.com>
2022-08-16 17:59:15 -0400

    Added Client and Server documentation.

--- HuffmanCompressionChat/src/HuffmanCoding.java
+++ HuffmanCompressionChat/src/HuffmanCoding.java
@@ -19,6 +19,9 @@ public class HuffmanCoding {
     }
 
     TreeNode rootNode = new TreeNode();
+    if(heap.size() <= 1) {
+      rootNode = heap.poll();
+    }
     while(heap.size() > 1) {
       TreeNode leftNode = heap.poll();
       TreeNode rightNode = heap.poll();
@@ -38,45 +41,40 @@ public class HuffmanCoding {
   }
 
   public String decode(TreeNode rootNode, String encodedString) {
-    String originalString = "";
+    StringBuilder originalString = new StringBuilder();
     int i=0, j=0;
-    for(; j < encodedString.length(); j++) {
-      String partOfString = "";
-      if(i == j) {
-        partOfString = encodedString.substring(i);
-      } else {
-        partOfString = encodedString.substring(i, j);
-      }
+    for(; j <= encodedString.length(); j++) {
+      String partOfString = encodedString.substring(i, j);
       Character charPart = decodeTheCharacterFromTheTree(rootNode, 0, partOfString);
       if(charPart != null) {
-        originalString += charPart;
+        originalString.append(charPart);
         i = j;
       }
     }
-    return originalString;
+    return originalString.toString();
   }
 
   private void convertEncodingTreeToHashMap(TreeNode rootNode, String accumulate, Map<Character, String> decodingMap) {
-      if(rootNode == null) {
-        return;
-      }
-      if(rootNode.right == null && rootNode.left == null) {
-         decodingMap.put(rootNode.character, accumulate);
-         return;
-      }
-      if(rootNode.left != null) {
-        convertEncodingTreeToHashMap(rootNode.left, accumulate + '0', decodingMap);
-      }
-      if(rootNode.right != null) {
-        convertEncodingTreeToHashMap(rootNode.right, accumulate + '1', decodingMap);
-      }
+    if(rootNode == null) {
+      return;
+    }
+    if(rootNode.right == null && rootNode.left == null) {
+      decodingMap.put(rootNode.character, accumulate);
+      return;
+    }
+    if(rootNode.left != null) {
+      convertEncodingTreeToHashMap(rootNode.left, accumulate + '0', decodingMap);
+    }
+    if(rootNode.right != null) {
+      convertEncodingTreeToHashMap(rootNode.right, accumulate + '1', decodingMap);
+    }
   }
   private Character decodeTheCharacterFromTheTree(TreeNode rootNode, int i, String partOfString) {
     if(rootNode == null) {
       return null;
     }
     if(i == partOfString.length() && rootNode.character != null) {
-        return rootNode.character;
+      return rootNode.character;
     } else if(i == partOfString.length()){
       return null;
     }
